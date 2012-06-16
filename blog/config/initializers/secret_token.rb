8cb3fdd36aa550458c6d063b53f0caf58e37ee12
gothicfairies2569 <gothicfairies2569@gmail.com>
2012-06-16 16:53:58 -0500

    bloooggggg

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '08ae3482713d4a822e07d3daa93deca81372bcd9d90cd4754f33e3dab566fd2b3aa2ab7477c3a33a7993227fd71003406382868b63c3a2a053e80d03d3715bdf'
